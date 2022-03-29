c9a0a8228fcc9e4fad9d71acb572ff36f2533c57
LuisMCota <luis.f.m.c@outlook.com>
2022-03-29 11:06:08 -0600

    commit bitches

---
+++ Odd.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+int odd(int arr[], int len) {
+    
+    for (int index = 0; index < len; index ++) {
+
+    int numberToTest = index;
+    int result = numberToTest % 2;
+    bool hasModule = (result > 0);
+        if (hasModule){
+            
+        }else{
+            
+        }
+        
+    }
+return 0;
+}
