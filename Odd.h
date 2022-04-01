cbc589ba969b4eca670902a748168cdc5be0a511
LuisMCota <luis.f.m.c@outlook.com>
2022-04-01 08:03:28 -0600

    fix: deleted no necesary code

--- Odd.h
+++ Odd.h
@@ -1,18 +1,21 @@
-#include <stdio.h>
 #include <stdbool.h>
-int odd(int arr[], int len) {
-    
-    for (int index = 0; index < len; index ++) {
+#include <stdlib.h>
 
+int odd(int array[], int len) {
+    int count   = 0;
+    for (int index = 0; index < len; index ++) {
+    
     int numberToTest = index;
-    int result = numberToTest % 2;
+    float result = numberToTest % 2;
     bool hasModule = (result > 0);
+
         if (hasModule){
-            
-        }else{
+            count++;
+       }
+       else{
             
         }
         
     }
-return 0;
+    return count;
 }
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "Odd.h"
-
-int array[3] = {3,1,10};
-int main(){
-    int numbersOdd = odd(array, 3);
-
-}
