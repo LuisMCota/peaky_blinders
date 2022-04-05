807ba0a60ca84826dd17338246b9484e0236d354
VOC12 <2109155@upy.edu.mx>
2022-04-05 09:33:38 -0500

    add min

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "Odd.h"
 #include "find.h"
+#include "Min.h"
 
 int array[3] = {3,1,10};
 int main(){
     int numbersOdd = odd(array, 3);
     int numberToFind = find(array, 3, 1);
+    int minNumber = min(array, 10);
     printf("Odd %d", numbersOdd);
     printf("find %d", numberToFind);
+    printf("min %d", minNumber);
     
 return 0;
 }
